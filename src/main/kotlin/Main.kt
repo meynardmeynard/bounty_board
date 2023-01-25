5c4a7ea912fe9128b400ffd28739090407541289
meynardmeynard <jerichomm6@gmail.com>
2023-01-25 17:25:16 +0800

    challenge2_unicornHorn completed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,17 +5,24 @@ fun main(args: Array<String>) {
     println(HERO_NAME)
     var playerLevel = 4
     println(playerLevel)
-    var hasSteed = false;
-
+    var hasSteed = false
+    val pubName = "The Unicorn\'s Horn"
+    val publican = "Rome"
+    var heroGold = 50
+    val drinks: List<String> = listOf("Mead", "Wine", "La Croix")
 
     println("The hero embarks on her journey to locate the enchanted sword.")
     playerLevel++
     println(playerLevel)
     if (hasSteed) {
-        println(HERO_NAME+ " has steed")
+        println("$HERO_NAME has steed")
     }
     else {
-        println(HERO_NAME+ " doesn't have steed")
+        println("$HERO_NAME doesn't have steed")
     }
 
+    println("Welcome to $pubName, I\'m $publican your publican.")
+    println("$HERO_NAME has $heroGold gold")
+    println("Here's our available drinks")
+    for (d in drinks) { println("$d") }
 }
